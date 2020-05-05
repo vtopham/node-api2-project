44b214d6b83d39a7fda1f00571a9ff2d1aa3bd8d
Victoria  Topham <topham.victoria@gmail.com>
2020-05-05 11:43:08 -0700

    successful get for /api/posts created

--- post-routes/postRouter.js
+++ post-routes/postRouter.js
@@ -6,8 +6,14 @@ const db = require('../data/db.js')
 
 //Returns an array of all the post objects contained in the database.
 router.get('/',(req, res) => {
-    
+    db.find().then(posts => {
+        res.status(200).json({data: posts})
+    }).catch(_ => {
+        //If there's an error in retrieving the posts from the database
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
 })
+
 
 //Creates a post using the information sent inside the request body.
 router.post('/', (req, res) => {
@@ -31,6 +37,7 @@ router.post('/', (req, res) => {
     }
 })
 
+
 //Returns the post object with the specified id.
 router.get('/:id', (req, res) => {
 
@@ -49,12 +56,41 @@ router.delete('/:id', (req, res) => {
 
 //Returns an array of all the comment objects associated with the post with the specified id.
 router.get('/:id/comments', (req, res) => {
+    
 
 })
 
-//Creates a comment for the post with the specified id using information sent inside of the request body.
-router.post('/:id/comments', (req, res) => {
 
+//TODO: FIX Creates a comment for the post with the specified id using information sent inside of the request body.
+router.post('/:id/comments', (req, res) => {
+    //If the request body is missing the text property
+    if(!req.body.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    } else {
+        db.findById(req.params.id).then(
+            post => {
+                //If the post with the specified id is not found
+                if (post === []) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                }
+                //If the information about the comment is valid
+                const newComment = {
+                    text: req.body.text,
+                    post_id: post.id,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                }
+                insertComment(newComment).then(comment => {
+                    res.status(201).json({...newComment, id: comment.id})
+                }).catch( _ => {
+                    //If there's an error while saving the comment
+                    res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                })
+            }
+        ).catch( error => {
+            res.status(500).json({message: "error!", error: error})
+        })
+    }
 })
 
 
